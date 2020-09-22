1f2023ce667abd68b6bcd15b1280ca275e6758ea
Richard Supak <richard.supak@icloud.com>
2020-09-22 14:22:23 -0500

    Add solution for Decimal Comparator exercise

---
+++ DecimalComparator/src/com/rsupak/DecimalComparator.java
@@ -0,0 +1,8 @@
+package com.rsupak;
+
+public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
+        return (int)(number1 * 1000) == (int)(number2 * 1000);
+    }
+}

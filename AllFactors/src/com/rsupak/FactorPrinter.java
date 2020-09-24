2702baa64749b386c52b8b264eb698f9924ec1d0
Richard Supak <richard.supak@icloud.com>
2020-09-24 13:49:53 -0500

    Add solution for All Factors exercise

---
+++ AllFactors/src/com/rsupak/FactorPrinter.java
@@ -0,0 +1,15 @@
+package com.rsupak;
+
+public class FactorPrinter {
+    public static void printFactors(int number) {
+        if (number < 1) System.out.println("Invalid Value");
+        else {
+            int factor = 1;
+            while (factor < number) {
+                if (number % factor == 0) System.out.print(factor + " ");
+                factor++;
+            }
+            System.out.println(number);
+        }
+    }
+}

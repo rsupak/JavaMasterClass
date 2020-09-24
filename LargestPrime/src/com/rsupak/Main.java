0331dbde37a91459625ce03660df81eed1eab523
Richard Supak <richard.supak@icloud.com>
2020-09-24 16:40:40 -0500

    Add solution for Largest Prime

---
+++ LargestPrime/src/com/rsupak/Main.java
@@ -0,0 +1,8 @@
+package com.rsupak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Largest Prime: " + LargestPrime.getLargestPrime(21));
+    }
+}

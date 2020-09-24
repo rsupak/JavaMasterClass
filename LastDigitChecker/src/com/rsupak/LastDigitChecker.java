e44a02ac94013ca1bac4ce7fc828cc25a38c12d3
Richard Supak <richard.supak@icloud.com>
2020-09-24 13:31:35 -0500

    Add solution for LastDigitChecker exercise

---
+++ LastDigitChecker/src/com/rsupak/LastDigitChecker.java
@@ -0,0 +1,15 @@
+package com.rsupak;
+
+public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int num1, int num2, int num3) {
+        if (!isValid(num1) || !isValid(num2) || !isValid(num3)) return false;
+        num1 %= 10;
+        num2 %= 10;
+        num3 %= 10;
+        return (num1 == num2) || (num1 == num3) || (num2 == num3);
+    }
+
+    public static boolean isValid(int num) {
+        return (num >= 10 && num <= 1000);
+    }
+}

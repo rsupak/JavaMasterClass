ae2e863c694ad5dccc4e6e205d1111f7f2edc10c
Richard Supak <richard.supak@icloud.com>
2020-09-24 11:52:19 -0500

    Add solution for NumberPalindrome exercise

---
+++ NumberPalindrome/src/com/rsupak/Main.java
@@ -0,0 +1,13 @@
+package com.rsupak;
+
+import java.text.NumberFormat;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(NumberPalindrome.isPalindrome(11));
+        System.out.println(NumberPalindrome.isPalindrome(-1221));
+        System.out.println(NumberPalindrome.isPalindrome(707));
+        System.out.println(NumberPalindrome.isPalindrome(11212));
+    }
+}
---
+++ NumberPalindrome/src/com/rsupak/NumberPalindrome.java
@@ -0,0 +1,19 @@
+package com.rsupak;
+
+public class NumberPalindrome {
+    public static boolean isPalindrome(int number) {
+
+        number = (number > 0) ? number : -number;
+        int originalNumber = number;
+        int reverseNumber = 0;
+
+        while (number > 0) {
+            reverseNumber = (reverseNumber + number % 10);
+            reverseNumber *= (number < 10 ? 1 : 10);
+            number /= 10;
+        }
+        System.out.println(originalNumber);
+        System.out.println(reverseNumber);
+        return reverseNumber == originalNumber;
+    }
+}

4a183f8be0cf6beca6552b3db12a7f2e4fb5b6b6
Richard Supak <richard.supak@icloud.com>
2020-09-25 11:10:21 -0500

    Add solution for Person exercise

---
+++ OOPPart1/src/com/rsupak/Person.java
@@ -0,0 +1,42 @@
+package com.rsupak;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFullName() {
+        if (firstName == "" && lastName == "") return "";
+        if (firstName == "") return lastName;
+        if (lastName == "") return firstName;
+        return firstName + " " + lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = (age < 0 || age > 100) ? 0 : age;
+    }
+
+    public boolean isTeen() {
+        return (age > 12 && age < 20);
+    }
+}
